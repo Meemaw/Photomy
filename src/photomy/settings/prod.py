791ed3d774a459602ad7f0c1b3fc5861548796fa
Matej <ematej.snuderl@gmail.com>
2018-06-09 10:32:32 +0200

    Config files

--- src/photomy/settings/prod.py
+++ src/photomy/settings/prod.py
@@ -1,4 +1,28 @@
-from .base import *
+from .docker import *
 
+
+DEBUG = False
 
 EMAIL_CONFIRMATION_REDIRECT_URL = "http://photomy.si/confirmed"
+
+
+AWS_S3_CUSTOM_DOMAIN = os.environ['AWS_CLOUDFRONT_DOMAIN']
+AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PASSWORD': os.environ['POSTGRES_PASSWORD'],
+        'PORT': 5432,
+    }
+}
+
+
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_BROWSER_XSS_FILTER = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+X_FRAME_OPTIONS = 'DENY'
