ca1d6e4200256f858787bec5c28afcdcc14015cf
Matej <ematej.snuderl@gmail.com>
2018-06-27 18:29:00 +0200

    Fix linting

--- frontend/src/components/modules/Settings/GallerySettings/index.js
+++ frontend/src/components/modules/Settings/GallerySettings/index.js
@@ -1,10 +1,5 @@
 // @flow
 import * as React from 'react';
-import UserForm from '../../../UserForm';
-import SettingsTab from '../../../common/SettingsTab';
-import withWidth from '../../../../hocs/WithWidth';
-import AvatarUpload from '../../../AvatarUpload';
-import { List } from 'semantic-ui-react';
 import type { User } from '../../../../meta/types/User';
 
 type Props = { authUser: User, width: number };
