9704f3c2247e65f61d78843ce044d9c6ab1af096
Matej <ematej.snuderl@gmail.com>
2018-08-10 09:39:41 +0200

    [ci skip] export

--- frontend/src/test/utils/index.tsx
+++ frontend/src/test/utils/index.tsx
@@ -1,3 +1,6 @@
+import assertAfterTimeout from './assertAfterTimeout';
+
 export { default as sandbox } from './sandbox';
 export { default as assertWithTimeout } from './assertWithTimeout';
 export { default as withRouter } from './router';
+export { default as assertAfterTimeout } from './assertAfterTimeout';
